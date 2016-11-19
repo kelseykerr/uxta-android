b8af44be77d78e9fbbced23b48c9301813c45d15
Kelsey Kerr <kelsey.kerr@washpost.com>
2016-11-19 14:34:47 -0500

    take out extra class

--- app/src/main/java/superstartupteam/nearby/TextInputLayout.java
+++ app/src/main/java/superstartupteam/nearby/TextInputLayout.java
@@ -5,7 +5,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 
 /**
  * Created by kerrk on 11/19/16.
